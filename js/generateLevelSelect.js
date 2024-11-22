fe255258b7f0d3a80a114031e21cdebb66431048
yamato3010 <yama3to10yama3@gmail.com>
2024-11-22 15:54:47 +0900

    feat: ✨ レベル選択画面を自動生成できるように

---
+++ js/generateLevelSelect.js
@@ -0,0 +1,67 @@
+fetch("level/levels.json")
+  .then(response => response.json())
+  .then(data => {
+
+    // 要素を生成する関数
+    function createCard(level) {
+      // カードのdiv要素を作成
+      const cardDiv = document.createElement('div');
+      cardDiv.className = 'card';
+      cardDiv.setAttribute('data-toggle', 'tooltip');
+      cardDiv.setAttribute('data-bs-placement', 'bottom');
+      cardDiv.setAttribute('data-bs-custom-class', 'ctm-tooltip');
+      cardDiv.setAttribute('title', level.description);
+      cardDiv.style.width = '15rem';
+
+      // リンク要素を作成
+      const link = document.createElement('a');
+      link.href = `level/game.html?id=${level.id}`;
+      link.className = 'btn btn-outline-primary';
+      link.type = 'button';
+
+      // 画像要素を作成
+      const img = document.createElement('img');
+      img.src = `./img/level_icon/${level.image}`;
+      img.alt = 'カードの画像';
+      img.className = 'card-img-top img-responsive';
+
+      // カードボディのdiv要素を作成
+      const cardBody = document.createElement('div');
+      cardBody.className = 'card-body';
+
+      // カードテキストのh5要素を作成
+      const cardText = document.createElement('h5');
+      cardText.className = 'card-text';
+      cardText.textContent = level.levelSelectName;
+
+      // カードボディにテキストを追加
+      cardBody.appendChild(cardText);
+
+      // リンクに画像とカードボディを追加
+      link.appendChild(img);
+      link.appendChild(cardBody);
+
+      // カードのdivにリンクを追加
+      cardDiv.appendChild(link);
+
+      return cardDiv;
+    }
+
+    // JSONデータを元に要素を生成して配置する
+    data.levels.forEach(level => {
+      if (level.level === 1) {
+        const level1Div = document.querySelector('.level-1');
+        const card = createCard(level);
+        level1Div.appendChild(card);
+      } else if (level.level === 2) {
+        const level2Div = document.querySelector('.level-2');
+        const card = createCard(level);
+        level2Div.appendChild(card);
+      }
+    });
+    // Tooltipを初期化
+    document.querySelectorAll('[data-toggle="tooltip"]').forEach(function (tooltipElement) {
+      new bootstrap.Tooltip(tooltipElement);
+    });
+  })
+  .catch(error => console.error("Error loading JSON data:", error));
